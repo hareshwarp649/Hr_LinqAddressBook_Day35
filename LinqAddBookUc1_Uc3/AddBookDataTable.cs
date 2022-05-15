367f38f3436963118865a87c9d43240f3b2bd68c
Harry <hareshwarp649>
2022-05-15 15:22:00 +0530

    [HRPatil]Added: Ability to create,Insert, new attrbute in addBook dataTable

---
+++ LinqAddBookUc1_Uc3/AddBookDataTable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAddBookUc1_Uc3
+{
+    internal class AddBookDataTable
+    {
+        public void AddToTable(DataTable addressBook)
+        {
+            addressBook.TableName = "AddressBook";
+            //Adding FirstName ,LastName,Address,City,State,PhoneNumber and Email.
+            addressBook.Rows.Add("Manoj", "Thiparapu", "25-4-710", "Kazipet", "Telangana", "8106529025", "manojthiparapu@gmail.com");
+            addressBook.Rows.Add("Eren", "Jeager", "Shinsengumi", "Wall Maria", "Attak on Titan", "7958977310", "erenjeager@gmail.com");
+            addressBook.Rows.Add("Sasuke", "Uchiha", "4-3-333", "Leaf Village", "Naruto", "9106529025", "schihasasuke@gmail.com");
+            addressBook.Rows.Add("Kamado", "Tanjiro", "mt Kumotori", "Okutama", "Demon Slayer", "7578977310", "kamadoTanjiro@gmail.com");
+
+            foreach (DataRow row in addressBook.Rows)
+            {
+                string firstName = row["First Name"].ToString();
+                string lastName = row["Last Name"].ToString();
+                string address = row["Address"].ToString();
+                string city = row["City"].ToString();
+                string state = row["State"].ToString();
+                string phoneNumber = row["Phone Number"].ToString();
+                string email = row["Email"].ToString();
+                Console.WriteLine("\nFirst Name : " + firstName + ", Last Name : " + lastName + ", Address : " + address + ", City : " + city + ", State : " + state +
+                    ", Phone Number : " + phoneNumber + ", Email : " + email);
+            }
+        }
+    }
+}
---
+++ LinqAddBookUc1_Uc3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAddBookUc1_Uc3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Address Book Linq");
+            AddBookDataTable adb = new AddBookDataTable();
+            //UC 1 and 2 Create Address Book DataTable
+            DataTable addressBook = new DataTable();
+            addressBook.TableName = "AddressBook";
+            addressBook.Columns.Add("First Name");
+            addressBook.Columns.Add("Last Name");
+            addressBook.Columns.Add("Address");
+            addressBook.Columns.Add("City");
+            addressBook.Columns.Add("State");
+            addressBook.Columns.Add("Phone Number");
+            addressBook.Columns.Add("Email");
+            Console.WriteLine("\nAddress Book Data Table Created");
+
+            //UC 3 Insert
+            adb.AddToTable(addressBook);
+
+        }
+    }
+}
